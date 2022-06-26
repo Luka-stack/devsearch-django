96f2b9462cbfea35491aa74f6ea5d300d36baceb
lkrra <lkr.rafa@gmail.com>
2022-06-26 22:30:44 +0200

    POSTGRES config

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -92,10 +92,21 @@ WSGI_APPLICATION = 'devsearch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'devsearch',
+        'USERNAME': 'postgres',
+        "PASSWORD": 'postgres',
+        'HOST': 'localhost',
+        'PORT': 4321
     }
 }
 
